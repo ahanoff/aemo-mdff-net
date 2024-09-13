f16827feee61f0a55adabc6afadd498d2a68a002
Akhan Zhakiyanov <ahanoff@gmail.com>
2024-09-13 16:55:05 +0800

    feat: add NMI data details record

---
+++ src/AEMO.MDFF/NEM12/NMIDataDetailsRecord.cs
@@ -0,0 +1,17 @@
+using AEMO.MDFF.Abstractions;
+
+namespace AEMO.MDFF.NEM12;
+
+public class NMIDataDetailsRecord : IMdffRecord
+{
+    public string RecordIndicator => "200";
+    public string NMI { get; set; }
+    public string NMIConfiguration { get; set; }
+    public string RegisterId { get; set; }
+    public string NMISuffix { get; set; }
+    public string MDMDataStreamIdentifier { get; set; }
+    public string MeterSerialNumber { get; set; }
+    public string UOM { get; set; }
+    public int IntervalLength { get; set; }
+    public DateTimeOffset NextScheduledReadDate { get; set; }
+}
